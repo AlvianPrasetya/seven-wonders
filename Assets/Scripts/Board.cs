70329c778e89456b718bba3244387683c987a70d
unknown <alvian.better@gmail.com>
2019-03-12 00:31:25 +0800

    Initial commit

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Board : MonoBehaviour {
+
+	[System.Serializable]
+	public struct WonderPosition {
+		// public Wonder wonder;
+		public Vector2 relativePosition;
+	}
+
+	public WonderPosition[] wonderPositions;
+
+}
