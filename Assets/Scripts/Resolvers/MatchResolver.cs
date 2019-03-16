3ccd8a9231c060ee0a06708e97977ac5ac839c65
unknown <alvian.better@gmail.com>
2019-03-17 02:25:06 +0800

    Remove SelectLeadersResolver due to lacking leaders assets

--- Assets/Scripts/Resolvers/MatchResolver.cs
+++ Assets/Scripts/Resolvers/MatchResolver.cs
@@ -5,7 +5,6 @@ public class MatchResolver : IResolvable {
 	
 	public IEnumerator Resolve() {
 		GameManager.Instance.EnqueueResolver(new PreGameResolver(), 2);
-		GameManager.Instance.EnqueueResolver(new SelectLeadersResolver(), 2);
 		GameManager.Instance.EnqueueResolver(new Age1Resolver(), 2);
 		GameManager.Instance.EnqueueResolver(new Age2Resolver(), 2);
 		GameManager.Instance.EnqueueResolver(new Age3Resolver(), 2);
