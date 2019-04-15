5c6f32e4b8c62717378dd1d56ead23834f605818
unknown <alvian.better@gmail.com>
2019-04-15 22:39:01 +0800

    Destroy leader cards when discarded to prevent digging of leader cards

--- Assets/Scripts/Game/Collections/DiscardPile.cs
+++ Assets/Scripts/Game/Collections/DiscardPile.cs
@@ -1,8 +1,19 @@
 using System.Collections;
+using UnityEngine;
 
 public class DiscardPile : Deck {
 
 	public override IEnumerator Push(Card card) {
+		if (card.GetType() == typeof(LeaderCard)) {
+			// Is a leader card, destroy upon reaching discard pile
+			Coroutine pushLeaderCard = StartCoroutine(base.Push(card));
+			Pop();
+			yield return pushLeaderCard;
+
+			Destroy(card.gameObject);
+			yield break;
+		}
+
 		yield return base.Push(card);
 
 		// Set discarded cards as virtually free
