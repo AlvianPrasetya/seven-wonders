2f1ee23acbb0f52cbfc07bbb8e8b26db372beb53
Calvin Tantio <tantioc@seagroup.com>
2019-04-20 13:52:50 +0800

    Implement gain victory points if more than neighbours effect

---
+++ Assets/Scripts/Game/Effects/GainPointsIfCoinsMoreThanNeighboursOnBuild.cs
@@ -0,0 +1,20 @@
+﻿public class GainPointsIfCoinsMoreThanNeighboursOnBuild : OnBuildEffect {
+	public PointType pointType;
+	public int victoryPoints;
+
+	public override void Effect(Player player) {
+		GainPointsResolver.Count count = () => {
+			int playerCoinCount = player.bank.Count;
+			int westNeighbourCoinCount = player.Neighbours[Direction.West].bank.Count;
+			int eastNeighbourCoinCount = player.Neighbours[Direction.East].bank.Count;
+
+			return  ((playerCoinCount > westNeighbourCoinCount) &&  
+					(playerCoinCount > eastNeighbourCoinCount)) ? victoryPoints : 0;
+		};
+
+		GameManager.Instance.EnqueueResolver(
+			new GainPointsResolver(player, pointType, count),
+			Priority.GainPoints
+		);
+	}
+}
